b6d1b413bff7178a84ea94f029f1535bac427ac9
Jeff Sapotille <you@example.com>
2019-09-24 13:42:43 +0200

    refactorisation du code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: false });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} user={this.state.users} />
         </div>
       </div>
     );
