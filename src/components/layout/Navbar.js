eab2f40191d687a59d13b3fa835a75f3ad1530ad
Jeff Sapotille <you@example.com>
2019-10-22 15:15:30 +0200

    configuration react-router

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
   return (
     <nav className="navbar bg-primary">
       <h1>
-        <i className={icon}> {title}</i>
+        <i className={icon}> {title} </i>
       </h1>
+      <ul>
+        <li>
+          <Link to="/"> Accueil </Link>
+        </li>
+        <li>
+          <Link to="/About"> A Propos </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>Cette application </h1>
+      <p>Est une app qui cherche les utilisateurs github</p>
+      <p>Version 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
